e0a3324fcae843ae4450c314163a3ee9f342c445
Vinicius Rodrigues <vini.rodrigues.90@gmail.com>
2016-11-13 16:11:06 -0200

    trial share button

--- src/pages/poster/poster.ts
+++ src/pages/poster/poster.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, Platform } from 'ionic-angular';
+
+
+// interface GoogleAnalyticsPlugin {
+    // startTrackerWithAccountID(accountId: string): void;
+// }
+
+// interface Plugins {
+    // googleAnalyticsPlugin: GoogleAnalyticsPlugin;
+// }
+
 
 /*
   Generated class for the Poster page.
@@ -14,8 +24,14 @@ import { NavController, NavParams } from 'ionic-angular';
 export class Poster {
   public person;
   public posterUrl;
+  public platform;
+  // public window: any;
+  // public window: any;
 
-  constructor(public navCtrl: NavController, private navParams: NavParams) {
+  constructor(public navCtrl: NavController, private navParams: NavParams, platform: Platform) {
+  // constructor(public navCtrl: NavController, private navParams: NavParams, platform: Platform) {
+    // this.window = window;
+    this.platform = platform;
     this.person = navParams.get('person');
     this.posterUrl = `http://162.243.118.247${this.person.poster.url}`;
   }
@@ -24,4 +40,17 @@ export class Poster {
     console.log('Hello Poster Page');
   }
 
+  share() {
+    // interface Window {
+    //   plugins: any;
+    // }
+
+    this.platform.ready().then(() => {
+      // if(this.window.plugins.socialsharing) {  
+      if((<any>window).plugins.socialsharing) {  
+          (<any>window).plugins.socialsharing.share("message hehe", null, null, null);
+      }
+      // console.log();
+    });
+  }
 }
