03a2cf8e98fdac49dd4b567c1da258604425df75
Vinicius Rodrigues <vini.rodrigues.90@gmail.com>
2016-11-13 23:23:26 -0200

    Fix share poster feature

--- src/pages/poster/poster.ts
+++ src/pages/poster/poster.ts
@@ -1,16 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, Platform } from 'ionic-angular';
 
-
-// interface GoogleAnalyticsPlugin {
-    // startTrackerWithAccountID(accountId: string): void;
-// }
-
-// interface Plugins {
-    // googleAnalyticsPlugin: GoogleAnalyticsPlugin;
-// }
-
-
 /*
   Generated class for the Poster page.
 
@@ -25,12 +15,8 @@ export class Poster {
   public person;
   public posterUrl;
   public platform;
-  // public window: any;
-  // public window: any;
 
   constructor(public navCtrl: NavController, private navParams: NavParams, platform: Platform) {
-  // constructor(public navCtrl: NavController, private navParams: NavParams, platform: Platform) {
-    // this.window = window;
     this.platform = platform;
     this.person = navParams.get('person');
     this.posterUrl = `http://162.243.118.247${this.person.poster.url}`;
@@ -41,16 +27,10 @@ export class Poster {
   }
 
   share() {
-    // interface Window {
-    //   plugins: any;
-    // }
-
     this.platform.ready().then(() => {
-      // if(this.window.plugins.socialsharing) {  
       if((<any>window).plugins.socialsharing) {  
-          (<any>window).plugins.socialsharing.share("message hehe", null, null, null);
+          (<any>window).plugins.socialsharing.share("Compartilhe o cartaz com seus conhecidos!", null, this.posterUrl, null);
       }
-      // console.log();
     });
   }
 }
